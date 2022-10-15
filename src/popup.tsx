70712b703ed7bbf3c7ebb024053f9e7c442d0034
Aradhana <58147211+AradhanaChhabra@users.noreply.github.com>
2022-10-15 16:55:27 +0530

    feat(chrome-ext): setup for extension

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,17 +1,29 @@
-import React, { useEffect, useState,useRef } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { Button, ButtonContainer, Container, Display, LookupButton, LookupInput, LookupWrapper, Message, ModalInput, TextArea, ValueFound } from "./PopupElements";
+import {
+  Button,
+  ButtonContainer,
+  Container,
+  Display,
+  LookupButton,
+  LookupInput,
+  LookupWrapper,
+  Message,
+  ModalInput,
+  TextArea,
+  ValueFound,
+} from "./PopupElements";
 
 const Popup = () => {
   const [message, setMessage] = useState([false, ""]);
   const [lookupValue, setLookUpValue] = useState({
     exist: false,
-    value:""
+    value: "",
   });
   const [copyBtnAtt, setCopyBtnAtt] = useState({
     innerText: "Copy to clipboard",
     disabled: false,
-  })
+  });
 
   const shortcutRef = React.useRef<HTMLInputElement>(null);
   const snippetTextRef = React.useRef<HTMLTextAreaElement>(null);
@@ -22,57 +34,61 @@ const Popup = () => {
     const key = shortcutRef.current?.value;
     const value = snippetTextRef.current?.value;
     if (key?.trim() && value?.trim()) {
-      chrome.storage.sync.set({ [key]: value }, () => setMessage([true,"variable has been set"]));
+      chrome.storage.sync.set({ [key]: value }, () =>
+        setMessage([true, "variable has been set"])
+      );
+    } else {
+      setMessage([false, "name or value cannot be empty"]);
     }
-    else {
-      setMessage([false,"name or value cannot be empty"])
-    }
-  }
+  };
 
   const editSnippetHandler = () => {
     chrome.tabs.create({ url: "variables.html" });
-  }
+  };
   const findSnippetHandler = () => {
     // tryGetValue()
     const key = findValueRef.current?.value;
     if (key?.trim()) {
-      chrome.storage.sync.get(key, function(result) {
-          var key_value = Object.entries(result)[0]
-          if (key_value != undefined){ 
-            setLookUpValue({
-              exist: true,
-              value:key_value[1]
-              })
-          }
-
-          else {
-            setLookUpValue({
-              exist: false,
-              value:"variable not found"
-              })
-          }
-      })
-  }
-  }
+      chrome.storage.sync.get(key, function (result) {
+        var key_value = Object.entries(result)[0];
+        if (key_value != undefined) {
+          setLookUpValue({
+            exist: true,
+            value: key_value[1],
+          });
+        } else {
+          setLookUpValue({
+            exist: false,
+            value: "variable not found",
+          });
+        }
+      });
+    }
+  };
   const copyBtnHandler = () => {
     setCopyBtnAtt({
       innerText: "Copied.",
-      disabled: true
+      disabled: true,
     });
     copyToClipboard(lookupValue.value);
+  };
+  function copyToClipboard(text: string) {
+    window.navigator.clipboard
+      .writeText(text)
+      .then(() => {})
+      .catch((err) => {
+        console.error("Could not copy text: ", err);
+      });
   }
-function copyToClipboard(text:string) {
-    window.navigator.clipboard.writeText(text)
-    .then(() => {
-    })
-  . catch(err => {
-    console.error('Could not copy text: ', err);
-  });
-}
   return (
     <Container>
-      <form style={{"width":"100%"}}>
-        <ModalInput type="text" id="key" ref={shortcutRef} placeholder="Shortcut for the snippet"/>
+      <form style={{ width: "100%" }}>
+        <ModalInput
+          type="text"
+          id="key"
+          ref={shortcutRef}
+          placeholder="Shortcut for the snippet"
+        />
         <TextArea
           id="value"
           ref={snippetTextRef}
@@ -81,10 +97,14 @@ function copyToClipboard(text:string) {
         />
       </form>
       <ButtonContainer>
-        <Button type="button"  onClick={saveSnippetHandler}>Save</Button>
-        <Button type="button"  onClick={editSnippetHandler}>Edit Variables</Button>
+        <Button type="button" onClick={saveSnippetHandler}>
+          Save
+        </Button>
+        <Button type="button" onClick={editSnippetHandler}>
+          Edit Variables
+        </Button>
       </ButtonContainer>
-      <Message error={!message[0]}>{message[1]!=""&&message[1]}</Message>
+      <Message error={!message[0]}>{message[1] != "" && message[1]}</Message>
       <form>
         <LookupWrapper>
           <LookupInput
@@ -92,21 +112,25 @@ function copyToClipboard(text:string) {
             ref={findValueRef}
             placeholder="Quick Variable Lookup"
           />
-          <LookupButton
-            id="find"
-            type="button"
-            onClick={findSnippetHandler}>Find</LookupButton>
+          <LookupButton id="find" type="button" onClick={findSnippetHandler}>
+            Find
+          </LookupButton>
         </LookupWrapper>
       </form>
 
       <br />
-      
-      <ValueFound>
-        {lookupValue.value!="" && <>
-          <Display error={!lookupValue.exist}>{lookupValue.value}</Display>
-          {lookupValue.exist&&<Button onClick={copyBtnHandler} disabled={copyBtnAtt.disabled}>{copyBtnAtt.innerText}</Button>}
-        </>}
 
+      <ValueFound>
+        {lookupValue.value != "" && (
+          <>
+            <Display error={!lookupValue.exist}>{lookupValue.value}</Display>
+            {lookupValue.exist && (
+              <Button onClick={copyBtnHandler} disabled={copyBtnAtt.disabled}>
+                {copyBtnAtt.innerText}
+              </Button>
+            )}
+          </>
+        )}
       </ValueFound>
     </Container>
   );
